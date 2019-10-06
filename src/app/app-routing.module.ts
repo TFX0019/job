4b4688c367f65817fd3cda1bd945f462aed6907a
TFX0019 <1400kss@gmail.com>
2019-10-06 01:46:43 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'new', loadChildren: './pages/new/new.module#NewPageModule' },
 ];
 
 @NgModule({
